a96c4035ab1c8bbb49e51af9be1a628872d3e579
Romain Francois <romain@r-enthusiasts.com>
2016-08-28 12:27:34 +0200

    added documentation for flights data

---
+++ R/flights.R
@@ -0,0 +1,24 @@
+#' Aggregated flights information about flights leaving or arriving budapest
+#' 
+#' Aggregated flights information about flights leaving or arriving budapest between 2007 and 2012. 
+#' The data is originally from the satRday Budapest dataviz contest but has been reorganized slightly :
+#' flights are aggregated by city (regardless of the airport) and type (scheduled or non scheduled)
+#' 
+#' @source http://budapest.satrdays.org/#datavizcompo
+#' @format Data Frame with columns
+#' \describe{
+#' \item{year}{year, from 2007 to 2012}
+#' \item{month}{month. Numbers from 1 to 12}
+#' \item{country}{country}
+#' \item{city}{city}
+#' \item{direction}{Incoming (flights arriving budapest), or Outgoing (flights leaving budapest)}
+#' \item{passengers}{Total number of passengers}
+#' \item{weight}{Total cargo weight}
+#' \item{capacity}{Total seat capacity}
+#' \item{flights}{Number of flights}
+#' \item{scheduled}{number of scheduled flights}
+#' \item{non_scheduled}{number of non scheduled flights}
+#' }
+#' @examples 
+#'   flights
+"flights"
